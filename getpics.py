66366cfe3dd11352015d7a38e2e6552dfb5a0fe9
Pratishta Yerakala <ypradhi9@gmail.com>
2015-11-28 01:49:07 -0500

    finally go the right path files to use curl

--- getpics.py
+++ getpics.py
@@ -2,9 +2,13 @@ from astropy.io import fits
 from numpy import *
 import matplotlib.pyplot as plt
 
-d = fits.open("nsa_v0_1_2.fits")
+a_rawfile = fits.open('nsa_v0_1_2.fits')
+d = nsa_rawfile[1].data
+#print b.field('subdir')
+#print b[1].field('subdir')
 n = len(d)
+for i in range (0, n-1):
+ #   print "hi"
+     print ("curl sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/" + d[i].field('subdir') + "/" + d[i].field('iauname') + "-irg.cutout.jpg > " + d[i].field('iauname') + "-irg.cutout.jpg")
 
-for i in range (0,  (n-1)):
-    print ("curl sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/" + d[i].subdir + "/" + d[i].iauname + "-irg.cutout.jpg > " + d[i].iauname + "-irg.cutout.jpg"
 
