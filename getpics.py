9c62058f6b300452cc14d9ea8ff395f3522b792b
Pratishta Yerakala <ypradhi9@gmail.com>
2015-11-27 22:34:26 -0500

    middle of writing script

---
+++ getpics.py
@@ -0,0 +1,10 @@
+from astropy.io import fits 
+from numpy import *
+import matplotlib.pyplot as plt
+
+d = fits.open("nsa_v0_1_2.fits")
+n = len(d)
+
+for i in range (0,  (n-1)):
+    print ("curl sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/" + d[i].subdir + "/" + d[i].iauname + "-irg.cutout.jpg > " + d[i].iauname + "-irg.cutout.jpg"
+
